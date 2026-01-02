cba371e5b8b701fd7d5ac47ff99f5d8161fbef04
Caio Lucas <clucaszrtmain@gmail.com>
2026-01-01 22:13:24 -0300

    feat: Adiciona as validações de todos os campos de input, checkbox e radio buttons

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ const btnRados = document.querySelectorAll("input[type='radio']")
 const textArea = document.querySelector("#msg-area")
 const checkbox = document.querySelector("#checkbox-cosent")
 
+btnSubmit.addEventListener("click", function(){
+    validaFname()
+    validaLname()
+    validaEmail()
+    validaFormatacaoEmail()
+    validaRadios()
+    validaRadios()
+    validaTextArea()
+    validaTextArea()
+    validaCheckbox()
+})
+
+const validaFname = () => inputFname.value === '' ? errorFname.style.display = "block" : errorFname.style.display = "none"
+const validaLname = () => inputLname.value === '' ? errorLname.style.display = "block" : errorLname.style.display = "none"
+const validaEmail = () => inputEmail.value === '' ? errorEmail.style.display = "block" : errorEmail.style.display = "none"
+const validaFormatacaoEmail = () => inputEmail.value.includes("@gmail") === false ? invalidEmail.style.display = "block" : invalidEmail.style.display = "none"
+const validaRadios = () =>  btnRados[0].checked === false && btnRados[1].checked === false ? errorRadio.style.display = "block" : errorRadio.style.display = "none" 
+const validaTextArea = () =>  textArea.value === '' ? errorMessage.style.display = "block" : errorMessage.style.display = "none" 
+const validaCheckbox = () =>  checkbox.checked === false ? errorCheckbox.style.display = "block" :  errorCheckbox.style.display = "none" 
